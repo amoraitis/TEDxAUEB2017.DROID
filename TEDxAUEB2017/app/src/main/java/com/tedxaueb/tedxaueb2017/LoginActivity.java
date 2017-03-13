f3334c47e5eb0d99d1acbb9a59c474b16e535b27
amoraitis <tasosthira7@gmail.com>
2017-03-13 23:04:16 +0200

    Corrected share button for >=Marshmallow devices

--- TEDxAUEB2017/app/src/main/java/com/tedxaueb/tedxaueb2017/LoginActivity.java
+++ TEDxAUEB2017/app/src/main/java/com/tedxaueb/tedxaueb2017/LoginActivity.java
@@ -1,9 +1,12 @@
 package com.tedxaueb.tedxaueb2017;
 
 import android.animation.ObjectAnimator;
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.ContextWrapper;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -16,7 +19,9 @@ import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
+import android.preference.PreferenceManager;
 import android.provider.MediaStore;
+import android.support.annotation.RequiresApi;
 import android.support.v4.app.ShareCompat;
 import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
@@ -34,6 +39,7 @@ import android.widget.Toast;
 import com.bumptech.glide.load.resource.transcode.BitmapBytesTranscoder;
 import com.squareup.picasso.Picasso;
 import com.squareup.picasso.Target;
+import com.tedxaueb.tedxaueb2017.Manifest.permission;
 
 import java.io.File;
 import java.io.FileOutputStream;
@@ -48,13 +54,18 @@ import static com.microsoft.azure.mobile.MobileCenter.LOG_TAG;
  */
 
 public class LoginActivity extends AppCompatActivity {
+    String[] perms = {"android.permission.WRITE_EXTERNAL_STORAGE", "android.permission.READ_EXTERNAL_STORAGE"};
+
+    int permsRequestCode = 200;
     ImageView profpic;
+    private SharedPreferences sharedPreferences;
     ProgressBar shareprogress;
     Bitmap bmOverlay;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.loginlayout);
+        sharedPreferences= PreferenceManager.getDefaultSharedPreferences(this);
         Typeface iconFont = FontManager.getTypeface(getApplicationContext(), FontManager.FONTAWESOME);
         FontManager.markAsIconContainer(findViewById(R.id.spreadtheword),iconFont);
         profpic=(ImageView) findViewById(R.id.profpic);
@@ -98,6 +109,16 @@ public class LoginActivity extends AppCompatActivity {
                     @Override
                     public void onClick(View v) {
                         shareprogress.setVisibility(View.VISIBLE);
+                        if (shouldAskPermission()) {
+                            if(hasPermission(perms[0]) && hasPermission(perms[1])){
+
+                            }else{
+                                if(!hasPermission(perms[0]) && !hasPermission(perms[1])) {
+                                    requestPermissions(perms, permsRequestCode);
+                                }
+                            }
+
+                        }
                         FileOutputStream output;
                         Bitmap bitmap = ((BitmapDrawable) profpic.getDrawable()).getBitmap();
                         // Find the SD Card path
@@ -117,7 +138,6 @@ public class LoginActivity extends AppCompatActivity {
 
                             // Type of file to share
                             share.setType("image/png");
-
                             output = new FileOutputStream(file);
 
                             // Compress into png format image from 0% - 100%
@@ -125,24 +145,24 @@ public class LoginActivity extends AppCompatActivity {
                             output.flush();
                             output.close();
                             // Locate the image to Share
-                            if (android.os.Build.VERSION.SDK_INT <= Build.VERSION_CODES.KITKAT) {
+                            //if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
                                 Uri uri=null;
-                                if(!file.exists()){
+                                if(file.exists()){
                                     uri = Uri.fromFile(file);
                                 }else{
                                     uri = getTheLatestPic();
                                 }
 
-                                getApplicationContext().grantUriPermission("com.tedxaueb.tedxaueb2017",uri,Intent.FLAG_GRANT_READ_URI_PERMISSION);
+                                //getApplicationContext().grantUriPermission("com.tedxaueb.tedxaueb2017",uri,Intent.FLAG_GRANT_READ_URI_PERMISSION);
 
                                 // Pass the image into an Intnet
                                 share.putExtra(Intent.EXTRA_STREAM, uri);
 
                                 // Show the social share chooser list
                                 startActivity(Intent.createChooser(share, "Share your Mosaic with"));
-                            }else{
+                            //}else{
                                 Toast.makeText(getContext(),"Share your image from your Gallery",Toast.LENGTH_LONG).show();
-                            }
+                            //}
 
                         } catch (Exception e) {
                             e.printStackTrace();
@@ -187,5 +207,53 @@ public class LoginActivity extends AppCompatActivity {
                 mBitmap, "myMosaic", null);
 
         return Uri.parse(path);
+    }
+    private boolean shouldAskPermission(){
+
+        return(Build.VERSION.SDK_INT>=Build.VERSION_CODES.M);
+
+    }
+    @Override
+    public void onRequestPermissionsResult(int permsRequestCode, String[] permissions, int[] grantResults){
+
+        switch(permsRequestCode){
+
+            case 200:
+
+                boolean writeAccepted = grantResults[0]== PackageManager.PERMISSION_GRANTED;
+                if(writeAccepted)markAsAsked(perms[0]);
+                boolean readAccepted = grantResults[1]==PackageManager.PERMISSION_GRANTED;
+                if(readAccepted)markAsAsked(perms[1]);
+                break;
+
+        }
+    }
+    @SuppressLint("NewApi")
+    private boolean hasPermission(String permission){
+
+        if(canMakeSmores()){
+
+            return(checkSelfPermission(permission)==PackageManager.PERMISSION_GRANTED);
+
+        }
+
+        return true;
+
+    }
+    private boolean shouldWeAsk(String permission){
+
+        return (sharedPreferences.getBoolean(permission, true));
+
+    }
+
+
+
+    private void markAsAsked(String permission){
+
+        sharedPreferences.edit().putBoolean(permission, false).apply();
+
+    }
+    private boolean canMakeSmores() {
+        return(Build.VERSION.SDK_INT>Build.VERSION_CODES.LOLLIPOP_MR1);
     }
 }
